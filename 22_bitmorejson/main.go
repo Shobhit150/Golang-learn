164e75201e04081dceea9efe95527a75fc8f80b2
Shobhit150 <vshobhit150@gmail.com>
2025-11-07 04:18:06 +0530

    mod

--- 22_bitmorejson/main.go
+++ 22_bitmorejson/main.go
@@ -6,34 +6,34 @@ import (
 )
 
 type Course struct {
-	Name string `json:"course_name"`
-	Price int `json:"price"`
-	Platform string `json:"platform"`
-	Password string `json:"-"`
-	Tag []string `json:"tag,omitempty"`
+	Name     string   `json:"course_name"`
+	Price    int      `json:"price"`
+	Platform string   `json:"platform"`
+	Password string   `json:"-"`
+	Tag      []string `json:"tag,omitempty"`
 }
-func main()  {
-	DecodeJson()
+
+func main() {
+	EncodingJson()
 }
-func EncodingJson()  {
+func EncodingJson() {
 	lcoCourses := []Course{
-		{"React", 12, "udemy", "123", []string{"Web","js"}}, 
-		{"Angular", 22, "udem1", "123", []string{"Web","js"}},
+		{"React", 12, "udemy", "123", []string{"Web", "js"}},
+		{"Angular", 22, "udem1", "123", []string{"Web", "js"}},
 		{"Js", 13, "udmy", "123", nil},
 	}
 
 	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
-	if err!= nil {
+	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s",finalJson)
+	fmt.Printf("%s", finalJson)
 }
 
-
-func DecodeJson()  {
+func DecodeJson() {
 	jsonDataFromWeb := []byte(`
 		{
-            "course_name": "React",
+            "course_name123": "React",
             "price": 12,
             "platform": "udemy",
             "tag": [
@@ -49,7 +49,7 @@ func DecodeJson()  {
 		fmt.Println("Is is valid")
 		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
 		fmt.Printf("%#v", lcoCourse)
-	}else{
+	} else {
 		fmt.Println("Not valid")
 	}
 
@@ -61,7 +61,7 @@ func DecodeJson()  {
 	fmt.Printf("%#v", myonlineData)
 
 	fmt.Print("\n\n\n\n--------\n")
-	for k,v := range myonlineData {
+	for k, v := range myonlineData {
 		fmt.Printf("Key is %v and value is %v and Type is %T\n", k, v, v)
 	}
 
--- 23_mod/main.go
+++ 23_mod/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main()  {
 	fmt.Println("Hello Mod")
+	greeter()
+}
+
+func greeter() {
+	fmt.Println("Hey ther")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("<h1>Welcome to goLang</h1>"))
 }
